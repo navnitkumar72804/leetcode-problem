e5e055f1c4b3b1a1c61ff31e2a55493c15dca3fc
Navnit Kumar <navnitkumar72804@gmail.com>
2026-08-04 07:36:15 +0530

    Time: 134 ms (5.40%) | Memory: 46.9 MB (5.81%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,30 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-
-            // Push opening brackets
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            } 
-            // Handle closing brackets
-            else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-
-                char top = stack.pop();
-
-                if ((ch == ')' && top != '(') ||
-                    (ch == '}' && top != '{') ||
-                    (ch == ']' && top != '[')) {
-                    return false;
-                }
+       int n=s.length();
+        while(true){
+            if(s.contains("()")){
+                s=s.replace("()","");
             }
+            else if(s.contains("{}")){
+                s=s.replace("{}","");
+            }
+           else if(s.contains("[]")){
+                s=s.replace("[]","");
+            }else{
+           return s.isEmpty();
+           }
         }
-
-        // Stack must be empty at the end
-        return stack.isEmpty();
-    }
+         
+        
+}
 }
