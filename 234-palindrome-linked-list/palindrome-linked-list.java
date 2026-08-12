efbd470a7c41667eb0a76e2d31d85ff21ee6a338
Navnit Kumar <navnitkumar72804@gmail.com>
2026-08-13 02:01:16 +0530

    Time: 4 ms (66.28%) | Memory: 96.6 MB (30.94%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,42 +1,39 @@
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
-        }
 
-        // Step 1: Find middle
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // Step 2: Reverse second half
-        ListNode secondHalf = reverse(slow);
-
-        // Step 3: Compare both halves
-        ListNode firstHalf = head;
-        while (secondHalf != null) {
-            if (firstHalf.val != secondHalf.val) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
-
-    private ListNode reverse(ListNode head) {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
         ListNode prev = null;
-        while (head != null) {
-            ListNode next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
+        ListNode post=null;
+
+        while (curr!= null) {
+            post=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=post;
         }
+
         return prev;
+    }
+    
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode temp=reverseList(slow.next);
+        slow.next=temp;
+
+        ListNode p1=head;
+        ListNode p2=slow.next;
+        while(p2!=null){
+            if(p1.val!=p2.val) return false;
+            p1=p1.next;
+            p2=p2.next;
+                
+            
+        }
+        return true;
     }
 }
